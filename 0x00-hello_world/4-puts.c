e683fb76c6fc06af81b48e08dd1d40269286eb2d
ca11meC0Z <elmehdifattah6@gmail.com>
2023-06-15 17:46:25 -0700

    4-puts program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */ 
+
+int main(void)
+{
+	printf("programming is like building a multilingual puzzle"\n);
+	return (0);
+}
