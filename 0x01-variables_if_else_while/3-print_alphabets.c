70c25bc4c7807f7e4acc7fa9be4a5c4e7c612e30
ca11meC0Z <elmehdifattah6@gmail.com>
2023-06-17 04:49:09 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+	char ALPH;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	for (ALPH = 'A'; ALPH <= 'Z'; ALPH++)
+	{
+		putchar(ALPH);
+	}
+	putchar('\n');
+	return (0);
+}
