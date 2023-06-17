0b09f1c88de1b0efccf6d88f2f44ccc82570b209
ca11meC0Z <elmehdifattah6@gmail.com>
2023-06-17 06:31:19 -0700

    print combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 48; n <= 57; n++)
 	{
-		putchar (n);
-		putchar (',');
-		putchar (' ');
+		putchar(n);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
