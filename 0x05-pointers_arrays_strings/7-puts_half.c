48acbe0aabf7e7bc3e031db8ad507aa75eb2bdb4
ca11meC0Z <elmehdifattah6@gmail.com>
2023-06-27 17:22:24 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints last half of the string
+ * @str: string we are working on
+ **/
+void puts_half(char *str)
+{
+	int len = 0;
+	int m, n;
+
+	while (m = 0; str[m] != '\0'; m++)
+	{
+		len++
+	}
+	n = len / 2;
+	if len % 2 = 1
+		n = (len + 1) / 2;
+
+	for (m = n; str[m] != '\0'; m++)
+		_putchar(str[m]);
+		_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copies one string into another
+ * @dest: is the destination string
+ * @src: is the source string
+ * Return: source string input
+ **/
+char *_strcpy(char *dest, char *src)
+{
+	char *tmp = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	dest = '\0';
+	return (tmp);
+}
