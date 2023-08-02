9e94825f1afe6c63cb11215374e4be88b8d8480c
ca11meC0Z <elmehdifattah6@gmail.com>
2023-08-01 18:23:06 -0700

    sigly linked lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,11 +2,11 @@
 
 /**
  * sum_listint - Calculates the sum of all the data in a listint_t list.
- * @list_head: Pointer to the first node in the linked list.
+ * @head: Pointer to the first node in the linked list.
  *
  * Return: The resulting sum of all the data.
  */
-int sum_listint(listint_t *list_head)
+int sum_listint(listint_t *head)
 {
 	int sum = 0;
 	listint_t *temp = head;
